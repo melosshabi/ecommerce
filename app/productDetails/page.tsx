c6f0bb580ae54c00df50ebea7e2f31752d4a7699
Melos <melosshabi05@gmail.com>
2023-11-17 16:46:28 +0100

    Update

--- app/productDetails/page.tsx
+++ app/productDetails/page.tsx
@@ -30,6 +30,7 @@ export default function ProductDetails() {
       }
       setProduct(productData)
       setActiveImage(productData.pictures[0])
+      document.querySelector('.spinner-wrapper')?.classList.toggle("active-spinner")
     }
     fetchProduct()
   },[])
@@ -96,6 +97,9 @@ export default function ProductDetails() {
  
   return (
     <div className='product-page'>
+      <div className="spinner-wrapper active-spinner">
+      <svg className="spinner" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="white" d="M10.72,19.9a8,8,0,0,1-6.5-9.79A7.77,7.77,0,0,1,10.4,4.16a8,8,0,0,1,9.49,6.52A1.54,1.54,0,0,0,21.38,12h.13a1.37,1.37,0,0,0,1.38-1.54,11,11,0,1,0-12.7,12.39A1.54,1.54,0,0,0,12,21.34h0A1.47,1.47,0,0,0,10.72,19.9Z"><animateTransform attributeName="transform" dur="0.75s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></path></svg>
+      </div>
       <div className="fullscreen-image">
         <button className="switch-fullscreen-image-btns close-fullscreen-image-btn" onClick={() => toggleFullscreenImage(fullscreenActions.close)}>
           <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>

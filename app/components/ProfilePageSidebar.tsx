6374bf756b6496a9b9da0b42614a7e407eba1224
Melos <melosshabi05@gmail.com>
2023-12-19 21:26:06 +0100

    New media queries
    Wrote new media queries to make the website responsive on more devices

--- app/components/ProfilePageSidebar.tsx
+++ app/components/ProfilePageSidebar.tsx
@@ -19,46 +19,61 @@ export default function ProfilePageSidebar({activePage, hideSidebar} : ProfilePa
         document.querySelector(`.${activePage}-li svg`)?.classList.add('active-icon')
         return () => {
             document.querySelector(`.${activePage}-li`)?.classList.remove('active-li')
-        document.querySelector(`.${activePage}-li svg`)?.classList.remove('active-icon')
+            document.querySelector(`.${activePage}-li svg`)?.classList.remove('active-icon')
         }
     }, [activePage])
+
+    function toggleMobileSidebar(){
+            const sidebar = document.querySelector('.profile-page-sidebar')
+            if(sidebar?.classList.contains("active-mobile-sidebar")){
+                sidebar.classList.remove('active-mobile-sidebar')
+            }else{
+                sidebar?.classList.add('active-mobile-sidebar')
+            }
+    }
+
   return (
     <div className={`profile-page-sidebar ${hideSidebar ? 'hidden-profile-sidebar' : ""}`}>
+                <div className="hamburger-btn" onClick={() => toggleMobileSidebar()}>
+                    <div id="ham-btn-line1"></div>
+                    <div id="ham-btn-line2"></div>
+                    <div id="ham-btn-line3"></div>
+                </div>
                 <ul>
-                <Link href="/userProfile/account">
+                <Link href="/userProfile/account" onClick={() => toggleMobileSidebar()}>
                     <li className="account-li">
                         <svg className="sidebar-icons user-icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"/></svg>
-                            Account Information
+                            <span>Account Information</span>
                     </li>
                 </Link>
                 
-                <Link href="/userProfile/cart">
+                <Link href="/userProfile/cart" onClick={() => toggleMobileSidebar()}>
                     <li className="cart-li">
                         <svg className="sidebar-icons orders-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                         <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
                         </svg>
-                        Cart
+                        <span>Cart</span>
                     </li>
                 </Link>
 
-                <Link href="/userProfile/wishlist">
+                <Link href="/userProfile/wishlist" onClick={() => toggleMobileSidebar()}>
                     <li className="wishlist-li">
                         <svg className='sidebar-icons orders-icon' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"/></svg>
-                        Wishlist
+                        <span>Wishlist</span>
                     </li>
                 </Link>
 
-                <Link href="/userProfile/userProducts">
+                <Link href="/userProfile/userProducts" onClick={() => toggleMobileSidebar()}>
                     <li className="userProducts-li">
                     <svg className="sidebar-icons box-icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M50.7 58.5L0 160H208V32H93.7C75.5 32 58.9 42.3 50.7 58.5zM240 160H448L397.3 58.5C389.1 42.3 372.5 32 354.3 32H240V160zm208 32H0V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V192z"/></svg>
-                        My Products
+                        <span>My Products</span>
                     </li>
                 </Link>
 
-                <Link href="/userProfile/userOrders">
+                <Link href="/userProfile/userOrders" onClick={() => toggleMobileSidebar()}>
                     <li className='userOrders-li'>
                     <svg className='sidebar-icons orders-icon' version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 115.35 122.88" ><g><path d="M25.27,86.92c-1.81,0-3.26-1.46-3.26-3.26s1.47-3.26,3.26-3.26h21.49c1.81,0,3.26,1.46,3.26,3.26s-1.46,3.26-3.26,3.26 H25.27L25.27,86.92L25.27,86.92z M61.1,77.47c-0.96,0-1.78-0.82-1.78-1.82c0-0.96,0.82-1.78,1.78-1.78h4.65c0.04,0,0.14,0,0.18,0 c1.64,0.04,3.1,0.36,4.33,1.14c1.37,0.87,2.37,2.19,2.92,4.15c0,0.04,0,0.09,0.05,0.14l0.46,1.82h39.89c1,0,1.78,0.82,1.78,1.78 c0,0.18-0.05,0.36-0.09,0.55l-4.65,18.74c-0.18,0.82-0.91,1.37-1.73,1.37l0,0l-29.18,0c0.64,2.37,1.28,3.65,2.14,4.24 c1.05,0.68,2.87,0.73,5.93,0.68h0.04l0,0h20.61c1,0,1.78,0.82,1.78,1.78c0,1-0.82,1.78-1.78,1.78H87.81l0,0 c-3.79,0.04-6.11-0.05-7.98-1.28c-1.92-1.28-2.92-3.46-3.92-7.43l0,0L69.8,80.2c0-0.05,0-0.05-0.04-0.09 c-0.27-1-0.73-1.69-1.37-2.05c-0.64-0.41-1.5-0.59-2.51-0.59c-0.05,0-0.09,0-0.14,0H61.1L61.1,77.47L61.1,77.47z M103.09,114.13 c2.42,0,4.38,1.96,4.38,4.38s-1.96,4.38-4.38,4.38s-4.38-1.96-4.38-4.38S100.67,114.13,103.09,114.13L103.09,114.13L103.09,114.13z M83.89,114.13c2.42,0,4.38,1.96,4.38,4.38s-1.96,4.38-4.38,4.38c-2.42,0-4.38-1.96-4.38-4.38S81.48,114.13,83.89,114.13 L83.89,114.13L83.89,114.13z M25.27,33.58c-1.81,0-3.26-1.47-3.26-3.26c0-1.8,1.47-3.26,3.26-3.26h50.52 c1.81,0,3.26,1.46,3.26,3.26c0,1.8-1.46,3.26-3.26,3.26H25.27L25.27,33.58L25.27,33.58z M7.57,0h85.63c2.09,0,3.99,0.85,5.35,2.21 s2.21,3.26,2.21,5.35v59.98h-6.5V7.59c0-0.29-0.12-0.56-0.31-0.76c-0.2-0.19-0.47-0.31-0.76-0.31l0,0H7.57 c-0.29,0-0.56,0.12-0.76,0.31S6.51,7.3,6.51,7.59v98.67c0,0.29,0.12,0.56,0.31,0.76s0.46,0.31,0.76,0.31h55.05 c0.61,2.39,1.3,4.48,2.23,6.47H7.57c-2.09,0-3.99-0.85-5.35-2.21C0.85,110.24,0,108.34,0,106.25V7.57c0-2.09,0.85-4,2.21-5.36 S5.48,0,7.57,0L7.57,0L7.57,0z M25.27,60.25c-1.81,0-3.26-1.46-3.26-3.26s1.47-3.26,3.26-3.26h50.52c1.81,0,3.26,1.46,3.26,3.26 s-1.46,3.26-3.26,3.26H25.27L25.27,60.25L25.27,60.25z"/></g></svg>
-                        Orders
+                        <span>Orders</span>
                     </li>
                 </Link>
                 </ul>

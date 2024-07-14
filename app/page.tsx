592ffb2516e9997ae98fa295d58d4e2c31d1ad0f
Melos Shabi <melosshabi05@gmail.com>
2024-07-15 01:36:41 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import getProducts from '@/lib/getProducts'
 import ImageSlider from './components/ImageSlider'
 import Product from './components/Product'
 import './styles/app.css'
+import React from 'react'
 
 export const revalidate = 10
 
@@ -16,8 +17,10 @@ export default async function Home() {
       <div className="products">
         {
           products.map((product, index) => (
-            // @ts-ignore
-            <Product productData={{...product._doc, index}} key={index}/>
+            <React.Fragment>
+              {/* @ts-ignore */}
+              <Product productData={{...product._doc, index}}/>
+            </React.Fragment>
           ))
         }
       </div>

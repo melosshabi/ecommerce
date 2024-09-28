ff177caf5dfee562ec5ff8eb1c9e0d7bf929eea4
Melos Shabi <melosshabi05@gmail.com>
2024-09-28 18:47:05 +0200

    Update

---
+++ app/api/getProducts/route.ts
@@ -0,0 +1,9 @@
+import connectToDb from "@/lib/mongodb";
+import productModel from "@/models/product";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    await connectToDb()
+    const products = await productModel.find({})
+    return NextResponse.json({products})
+}

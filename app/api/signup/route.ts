b52ac8ffcae4beb7e01b24711375125ad98e7b91
Melos Shabi <melosshabi05@gmail.com>
2024-10-01 13:25:32 +0200

    Rewrote a part of the signUp route to allow users to signup from the mobile app

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -2,6 +2,7 @@ import connectToDb from "@/lib/mongodb"
 import { hashPassword } from "@/lib/bcrypt"
 import userModel from "@/models/user"
 import { NextResponse } from "next/server"
+import { encrypt } from "@/lib/authLib"
 
 export async function POST(req:Request){
     const data = await req.json()
@@ -26,19 +27,14 @@ export async function POST(req:Request){
     }
 
     const hashedPassword = await hashPassword(data.password)
-
-    let newUser
+    const mobile = req.headers.get('mobile')
     try{
-        newUser = await userModel.create({...data, password:hashedPassword})
-    }catch(err:any){
-        console.log(err)
-        if(err._message === "User validation failed"){
-            return NextResponse.json({
-                errorMessage:"Please fill out the form",
-                errorCode:"incomplete-form"
-            }, {status:400})
+        const newUser = await userModel.create({...data, password:hashedPassword})
+        if(mobile){
+            const {_id, username, email, profilePictureUrl} = newUser._doc
+            const session = await encrypt({_id, username, email, profilePictureUrl})
+            return NextResponse.json({session})
         }
-    }
         const passwordLessUser = {
             ...newUser._doc
         }
@@ -49,4 +45,13 @@ export async function POST(req:Request){
         messageCode:"account-created",
         user:passwordLessUser
     })
+    }catch(err:any){
+        console.log(err)
+        if(err._message === "User validation failed"){
+            return NextResponse.json({
+                errorMessage:"Please fill out the form",
+                errorCode:"incomplete-form"
+            }, {status:400})
+        }
+    }
 }

777d6f668e23d076310f4f6b8286542c0ea43201
Melos Shabi <melosshabi05@gmail.com>
2024-10-09 22:49:49 +0200

    Rewrote partt of the updateUser route to allow requests from mobile devices

--- app/api/updateUser/route.ts
+++ app/api/updateUser/route.ts
@@ -1,12 +1,11 @@
 import connectToDb from "@/lib/mongodb"
 import userModel from "@/models/user"
-import { writeFile } from "fs/promises"
 import { NextResponse } from "next/server"
-import {join} from 'path'
 import cloudinary from 'cloudinary'
-import { unlink } from "fs/promises"
 import { getServerSession } from "next-auth"
 import { nextAuthOptions } from "../auth/[...nextauth]/options"
+import { decrypt } from "@/lib/authLib"
+import { ObjectId } from "mongodb"
 
 cloudinary.v2.config({
     cloud_name:process.env.CLOUD_NAME,
@@ -15,20 +14,42 @@ cloudinary.v2.config({
 })
 
 export async function PATCH(req:Request){
+    const mobile = req.headers.get("Mobile")
+    const data = mobile ? await req.json() : await req.formData()
+    if(mobile){
+        const session = req.headers.get("Authorization")
+        const token = session?.split(" ")[1]
+        if(token){
+            const user = await decrypt(token)
+            if(!data.username || !data.email){
+                return NextResponse.json({errMsg:"fields-cannot-be-empty"}, {status:400})
+            }
+            await connectToDb()
+            await userModel.findOneAndUpdate({_id:new ObjectId(user._id as string)}, {
+                username:data.username,
+                email:data.email
+            })
+            return NextResponse.json({msg:'profile-updated'})
+        }
+    }
     const session = await getServerSession(nextAuthOptions)
     if(!session){
         return NextResponse.json({errMessage:"You need to sign in before making changes to your profile", errCode:"unauthenticated"}, {status:400})
     }
-    const data = await req.formData()
     if(data.get('profilePicture')?.valueOf() !== undefined){
         const newProfilePicture: File = data.get('profilePicture') as unknown as File
         const bytes = await newProfilePicture.arrayBuffer()
         const buffer = Buffer.from(bytes)
-        
-        const path = join(__dirname, '/../../../../../profilePictures', newProfilePicture.name)
-        await writeFile(path, buffer)
 
-        const cloudinaryRes = await cloudinary.v2.uploader.upload(path, {folder:'ecommerce/profilePictures', public_id:`ProfilePictureOf${session?.user.userDocId}`, overwrite:true})
+        const cloudinaryRes: any = new Promise((resolve, reject) => {
+            cloudinary.v2.uploader.upload_stream({folder:'ecommerce/products', public_id:`Product${new Date()}`}, (err, res) => {
+                if(err){
+                    console.log("ERR: ", err)
+                    reject(err)
+                }
+                return resolve(res)
+            }).end(buffer)
+        })
 
         await connectToDb()
         await userModel.findOneAndUpdate({userId:data.get('userId')}, {
@@ -36,7 +57,6 @@ export async function PATCH(req:Request){
             email:data.get('newEmail'),
             profilePictureUrl:cloudinaryRes.url
         }, {new:true})
-        await unlink(path)
     }else {
         await connectToDb()
         await userModel.findOneAndUpdate({userId:data.get('userId')}, {

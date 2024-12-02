c7172fd19dbcdf947bf2105aeed714dc845ec93a
Melos Shabi <melosshabi05@gmail.com>
2024-12-02 12:09:11 +0100

    Fixed user icon not showing on the review form when the user doesnt have a profile picture uploaded

--- app/components/ProductReviews.tsx
+++ app/components/ProductReviews.tsx
@@ -160,8 +160,8 @@ export default function ProductReviews({productId}: {productId:string}) {
             <div className="w-[100dvw] py-[10px] px-[2px] flex items-center justify-center bg-white shadow-[0_0_5px_black] my-8 sm:w-[90dvw] sm:m-auto sm:rounded-lg sm:my-5 lg:w-[70%] 2xl:w-[50%]">
             <form className="sm:w-[90%]" onSubmit={(e) => handleReviewSubmit(e)}>
                 <div className='flex'>
-                    <div className=" mx-8 w-28 h-28 rounded-full overflow-hidden inline-flex items-center">
-                        <Image src={session?.data?.user.image as string} alt={'User Profile Picture'} width={1000} height={1000} className='w-full'/>
+                    <div className=" mx-8 w-24 h-24 rounded-full overflow-hidden inline-flex items-center">
+                        <Image src={session?.data?.user.image || userIcon} alt={'User Profile Picture'} width={1000} height={1000} className='w-full'/>
                     </div>
                     <svg onClick={() => handleStarClick(1)}  className="stars star1 w-9 inline-block cursor-pointer hover:fill-lightOrange" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
